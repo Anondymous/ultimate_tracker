7ac6d3e68de59b3ddc7c1643ac3752ee4c001e33
Anand Subramani <aasubramani@gmail.com>
2013-08-05 12:58:37 -0700

    NSLog hello world

--- UltimateTracker/UltimateTracker/AppDelegate.m
+++ UltimateTracker/UltimateTracker/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"This is a test");
     return YES;
 }
 							
